117834ea934e4980e475971a0f7ef8ca7a8fa438
IcyRelic <william@icyrelic.com>
2024-04-11 16:11:01 -0400

    Kill/Revive All

---
+++ SpookSuite/Cheats/Players/KillAll.cs
@@ -0,0 +1,19 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Manager;
+using SpookSuite.Util;
+
+namespace SpookSuite.Cheats
+{
+    internal class KillAll : ExecutableCheat
+    {
+
+        public override void Execute()
+        {
+            foreach (Player p in GameObjectManager.players)
+            {
+                if (!p.data.dead) p.Reflect().Invoke("CallDie");
+            }
+        }
+
+    }
+}
---
+++ SpookSuite/Cheats/Players/ReviveAll.cs
@@ -0,0 +1,18 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Manager;
+
+namespace SpookSuite.Cheats
+{
+    internal class ReviveAll : ExecutableCheat
+    {
+
+        public override void Execute()
+        {
+            foreach (Player p in GameObjectManager.players)
+            {
+                if (p.data.dead) p.CallRevive();
+            }
+        }
+
+    }
+}
