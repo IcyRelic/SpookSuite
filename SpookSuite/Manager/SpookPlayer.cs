2a14b0c203d564b6e5e2d9841816219afbd28275
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2024-04-12 21:05:10 -0400

    partial player manager

---
+++ SpookSuite/Manager/SpookPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpookSuite.Manager
+{
+    internal class SpookPlayer
+    {
+        //instiantate with player from game's class
+        SpookPlayer(Player player)
+        {
+            gamePlayer = player; 
+            photonPlayer = player.refs.view.Owner;
+        }
+        //game player class
+        public Player gamePlayer { get; private set; }
+        //photon player class
+        public Photon.Realtime.Player photonPlayer { get; private set; }
+        
+        public bool isAlive = false;
+        public bool isSpawningBlacklisted = false;
+        public bool isSpawningFullyBlacklisted = false;
+
+        //call an rpc from the player
+        public void CallRPC(string name, Photon.Pun.RpcTarget target, params object[] args)
+        {
+            gamePlayer.refs.view.RPC(name, target, args);
+        }
+
+        public void OnReceiveRPC
+    }
+}
---
+++ SpookSuite/Manager/SpookPlayerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpookSuite.Manager
+{
+    internal class SpookPlayerManager
+    {
+        public static List<SpookPlayer> players = new List<SpookPlayer>();
+        public static List<SpookPlayer> deadPlayers = new List<SpookPlayer>();
+        public static List<SpookPlayer> alivePlayers = new List<SpookPlayer>();
+
+        public static void SetPlayerSpawnBlacklisted(SpookPlayer player)
+        {
+
+        }
+
+        public static bool GetPlayerSpawnBlacklisted(SpookPlayer player)
+        {
+            if (player.IsSpawningFullyBlackListed)
+                return false;
+
+            return false;
+        }
+    }
+}
