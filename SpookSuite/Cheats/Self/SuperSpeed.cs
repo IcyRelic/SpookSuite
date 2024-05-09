760413ead036bf0c36235dfa0b31665c51088349
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2024-05-09 06:48:32 -0400

    fixed OnEnable, OnDisable

--- SpookSuite/Cheats/Self/SuperSpeed.cs
+++ SpookSuite/Cheats/Self/SuperSpeed.cs
@@ -5,22 +5,17 @@ namespace SpookSuite.Cheats
     internal class SuperSpeed : ToggleCheat, IVariableCheat<float>
     {
         public static float Value = 60f;
-        private static bool wasEnabled = false;
+
         public override void Update()
         {
             if (Player.localPlayer is null) return;
 
-            if(Enabled) 
-            {
-                wasEnabled = true;
-                Player.localPlayer.gameObject.GetComponent<PlayerController>().movementForce = Value; 
-            }
-            else if (wasEnabled)
-            {
-                wasEnabled = false;
-                Player.localPlayer.gameObject.GetComponent<PlayerController>().movementForce = 10f;
-            }
+            Player.localPlayer.gameObject.GetComponent<PlayerController>().movementForce = Value; 
         }
 
+        public override void OnDisable()
+        {
+            Player.localPlayer.gameObject.GetComponent<PlayerController>().movementForce = 10f;
+        }
     }
 }
